71a07bbfa4372ef4e1349a0814c5c112814d5406
AJ <ajai.np@gmail.com>
2018-11-11 21:37:46 +1100

    Added placeholders for http verbs

--- Src/NodeJs/myproductapp/api/routes/products.js
+++ Src/NodeJs/myproductapp/api/routes/products.js
@@ -3,13 +3,23 @@ const router = express.Router();
 
 
 router.get('/', (req,res,next)=>{
-    res.status(200).json({message:"products get"});
+    res.status(200).json({message:"products get all"});
 });
 
 router.post('/', (req,res,next)=>{
-    res.status(200).json({message:"products post"});
+    res.status(200).json({message:"products posted"});
 });
 
+
+router.patch('/:productId', (req,res,next)=>{
+    const id=req.params.productId;
+    res.status(200).json({message:"products deleted", id:id});
+});
+
+router.delete('/:productId', (req,res,next)=>{
+    const id=req.params.productId;
+    res.status(200).json({message:"product deleted", id:id});
+});
 
 router.get('/:productId', (req,res,next)=>{
 
--- Src/NodeJs/myproductapp/app.js
+++ Src/NodeJs/myproductapp/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 
-const productsRoute = require('./api/routes/products')
-
+const productsRoute = require('./api/routes/products');
+const ordersRoute = require('./api/routes/orders');
 // app.use((req, res, next) => {
 //     res.status(200).json({ message: 'it works' });
 // });
 
 app.use('/products', productsRoute);
+app.use('/orders', ordersRoute);
 
 
 module.exports = app;
