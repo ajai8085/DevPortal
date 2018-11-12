e7e62f637a5a9583f9c0fae3a8804b18e4951357
AJ <ajai.np@gmail.com>
2018-11-12 20:06:14 +1100

    Cors package

--- Src/NodeJs/myproductapp/app.js
+++ Src/NodeJs/myproductapp/app.js
@@ -2,22 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const productsRoute = require('./api/routes/products');
 const ordersRoute = require('./api/routes/orders');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));//Only simple body for url encoded data
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    req.header('Access-Control-Allow-Origin', "*");
-    req.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-    if (req.method === 'OPTIONS') {
-        req.header('Access-Control-Allow-Mehtods', 'PUT, POST, PATCH, DELTE, GET');
-        return res.status(200).json({});
-    }
-});
+app.use(cors());
 
 app.use('/products', productsRoute);
 app.use('/orders', ordersRoute);
